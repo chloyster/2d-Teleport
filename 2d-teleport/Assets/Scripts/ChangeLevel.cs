a0b83d0e1e405d3d8d2547650bc6c4388952ba57
Gabriella Santiago <gabriellsantiago1@yahoo.com>
2019-01-24 14:14:32 -0800

    Added level 2, and a collider to switch to level 2

---
+++ 2d-teleport/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D gameObjectInformation)
+    {
+        Debug.Log("I'm in");
+        if (gameObjectInformation.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
