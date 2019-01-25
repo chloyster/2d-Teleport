4c5ca8240b122cd34a4b58c3a8fae54d39a3d292
duxlow <daytonmux@yahoo.com>
2019-01-24 23:58:53 -0800

    Added a 'gun' and projectiles.

---
+++ 2d-teleport/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float lifeTime = 1.5f;
+
+    //public GameObject destroyEffect;
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ 2d-teleport/Assets/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float offset;
+
+    public GameObject projectile;
+    public Transform shotPoint;
+
+    private float timeBtwnShots;
+    public float startTimeBtwShots;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwnShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwnShots = startTimeBtwShots;
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                int doNothing = 0;
+            }
+        }
+        else
+        {
+            timeBtwnShots -= Time.deltaTime;
+        }
+
+    }
+}
