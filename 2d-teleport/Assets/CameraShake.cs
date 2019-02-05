ef8312d0b469796578795ef17b34cf4e4d7d8a32
vernonchan <vernontchan@gmail.com>
2019-02-05 09:34:26 -0800

    updated camera shake

--- 2d-teleport/Assets/CameraShake.cs
+++ 2d-teleport/Assets/CameraShake.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//From YouTube tutorial: https://youtu.be/9A9yj8KnM8c
 public class CameraShake : MonoBehaviour
 {
     public float magnitude = 0.0f;
 
     public IEnumerator Shake (float duration, float magnitude)
     {
+        if (Mathf.Abs(this.magnitude) < 0.001f)
+        {
+            this.magnitude = magnitude;
+        }
         Vector3 originalPos = transform.localPosition;
 
         float elapsed = 0.0f;
@@ -25,5 +30,6 @@ public class CameraShake : MonoBehaviour
         }
 
         transform.localPosition = originalPos;
+        this.magnitude = 0.0f;
     }
 }
