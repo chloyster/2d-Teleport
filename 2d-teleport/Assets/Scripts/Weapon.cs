b7e8ee12f0a7c4174a83142532ab38d3fa522cf5
duxlow <daytonmux@yahoo.com>
2019-02-11 19:17:06 -0800

    Fixed aiming issues, updated projectile speed.

--- 2d-teleport/Assets/Scripts/Weapon.cs
+++ 2d-teleport/Assets/Scripts/Weapon.cs
@@ -18,7 +18,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
