f2ab7b7bc19bd06509c352078779613723a62a67
Christopher Gix <chrisgix97@gmail.com>
2019-02-21 10:50:33 -0800

    Added turret to chris scene with some logic

---
+++ 2d-teleport/Assets/Scripts/turretAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretAI : MonoBehaviour
+{
+
+    private bool inSight = false;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inSight)
+        {
+            Vector3 difference = target.transform.position - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            inSight = true;
+            target = collision.gameObject.transform;
+        }
+    }
+}
