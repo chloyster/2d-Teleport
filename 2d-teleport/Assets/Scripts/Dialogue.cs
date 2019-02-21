20d2897d3e6b53b9deefdacdbd13b0e6268200c1
Gabriella Santiago <gabriellsantiago1@yahoo.com>
2019-02-21 15:59:29 -0800

    made a dialogue system for intro cutscene

---
+++ 2d-teleport/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    // Gabriella wrote:
+
+    public Text textDisplay;
+    public string[] sentences;
+
+    public float typingSpeed;
+    private int index;
+
+    public GameObject continueButton;
+
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed); // check this
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+
+}
