9f32c0b804c3bfe37c2d145ae78c19fd6b3b5c3e
Gabriella Santiago <gabriellsantiago1@yahoo.com>
2019-02-26 10:23:39 -0800

    made changes to intro scene

---
+++ 2d-teleport/Assets/Scripts/IntroSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneManager : MonoBehaviour
+{
+
+    // gabriella wrote this...
+
+    public GameObject dialogueManager;
+    public Animator wakeUpanim;
+    
+    void Start()
+    {
+        dialogueManager.SetActive(false);
+    }
+
+    public void StartDialogue()
+    {
+        dialogueManager.SetActive(true);
+    }
+}
