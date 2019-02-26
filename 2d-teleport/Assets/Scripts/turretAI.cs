2ba032b2441c0a0fcb34c41f32341cad8196cf80
Christopher Gix <chrisgix97@gmail.com>
2019-02-26 10:30:38 -0800

    More to boss level

--- 2d-teleport/Assets/Scripts/turretAI.cs
+++ 2d-teleport/Assets/Scripts/turretAI.cs
@@ -8,23 +8,26 @@ public class turretAI : MonoBehaviour
     private bool inSight = false;
     private Transform target;
     public GameObject bullet;
+    private float timer = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inSight)
+        if (inSight && timer <= 0)
         {
             Vector3 difference = target.transform.position - transform.position;
             float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
             GameObject turrBullet = Instantiate(bullet, transform.position, transform.rotation);
+            timer = 1.0f;
         }
+        timer -= Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
