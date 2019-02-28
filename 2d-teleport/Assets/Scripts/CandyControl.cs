befebc51030777aa43ec2be540e3ad7207be3e14
Gabriella Santiago <gabriellsantiago1@yahoo.com>
2019-02-28 14:28:12 -0800

    added to milestone 3 and added candy

---
+++ 2d-teleport/Assets/Scripts/CandyControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2f;
+    public float maxRotation = 5f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameController.control.health += 2;
+            gameObject.SetActive(false);
+        }
+    }
+}
